70b97299be102e14901c8c5ffe24fb346c90e534
Victor Batata <victorbatata@itstransdata.com>
2025-10-21 08:23:25 -0300

    Finish organizing the project with vite

---
+++ chatbot-project/src/components/ChatInput.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react'
+import { Chatbot } from 'supersimpledev'
+import LoadingSpinnerGif from '../assets/loading-spinner.gif'
+import './ChatInput.css'
+
+function ChatInput({ chatMessages, setChatMessages }) {
+  const [inputText, setInputText] = useState('')
+
+  function saveInputText(event) {
+    setInputText(event.target.value)
+  }
+
+  async function sendMessage() {
+    setInputText('')
+
+    const newChatMessages = [
+      ...chatMessages,
+      {
+        message: inputText,
+        sender: 'user',
+        id: crypto.randomUUID()
+      }
+    ]
+
+    setChatMessages([
+      ...newChatMessages,
+      {
+        message: <img src={LoadingSpinnerGif} className="loading-spinner" />,
+        sender: 'robot',
+        id: crypto.randomUUID()
+      }
+    ])
+
+    const response = await Chatbot.getResponseAsync(inputText)
+    setChatMessages([
+      ...newChatMessages,
+      {
+        message: response,
+        sender: 'robot',
+        id: crypto.randomUUID()
+      }
+    ])
+  }
+
+  function handleKeyDown(event) {
+    if (event.key === 'Enter') {
+      sendMessage()
+    }
+  }
+
+  return (
+    <div className="chat-input-container">
+      <input
+        placeholder="Send a message to Chatbot"
+        size="30"
+        onChange={saveInputText}
+        onKeyDown={handleKeyDown}
+        value={inputText}
+        className="chat-input"
+      />
+      <button
+        onClick={sendMessage}
+        className="send-button"
+      >Send</button>
+    </div>
+  )
+}
+
+export default ChatInput
